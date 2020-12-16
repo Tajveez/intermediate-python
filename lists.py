068696a45ef6fc9057fc7681052ed0dd1d9bf1db
Tajveez <Tajveez786@gmail.com>
2020-12-16 23:37:59 +0500

    more about list

--- lists.py
+++ lists.py
@@ -15,3 +15,29 @@ for x in myList:
 
 if "banana" in myList:
     print('Yes')
+else:
+    print('No')
+
+print(len(myList3))
+
+myList.append("lemon")
+myList.insert(1, "blueberry")
+print(myList)
+
+# removing all element
+myList.pop()
+myList.remove("cherry")
+
+# Delete all item
+myList.clear()
+
+myList3.reverse()
+
+myList.sort()
+new_list = sorted(myList)  #sorted method
+
+myListNum = [0] * 5
+print(myListNum)
+
+myListNum2 = [1, 2, 3]
+new_num_list = myListNum + myListNum2
