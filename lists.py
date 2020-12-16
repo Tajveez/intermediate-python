4a234d3c70a3d5c22c101623b8434d749b9c701b
Tajveez <Tajveez786@gmail.com>
2020-12-16 23:26:16 +0500

    List

--- lists.py
+++ lists.py
@@ -6,3 +6,12 @@ print(myList2)
 
 myList3 = [5, True, "apple", "apple"]
 print(myList3)
+
+print(myList[2])
+print(myList[-1])
+
+for x in myList:
+    print(x)
+
+if "banana" in myList:
+    print('Yes')
